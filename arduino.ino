b1e54bcd9b3692832638ccbe965d4dd943b09434
CactusBT <151160190+CactusBT@users.noreply.github.com>
2026-01-17 14:20:39 +0100

    testfile4 motors only

--- arduino.ino
+++ arduino.ino
@@ -1,214 +1,91 @@
 #include <Servo.h>
 
-int stcp_Pin = 12;
-int shcp_Pin = 11;
-int ds_Pin   = 3;
-
-const int LED_PIN       = 13;
-const int espTriggerPin = 4;
-
 Servo motor1;
 Servo motor2;
 
-const int motorPin  = 10;
+const int motorPin1 = 10;
 const int motorPin2 = 9;
 
-const int RIGHT  = 180;
-const int LEFT   = 0;
+const int M1_LEFT  = 10;
+const int M1_RIGHT = 170;
 
-int currentAngle = 90;
-int direction = 1;
-unsigned long lastMoveTime = 0;
+const int M2_MIN = 30;
+const int M2_MAX = 120;
+
+const int M2_STEP = 10;
+
+const int M2_MODE_BOUNCE = 1;
+
+int m1 = M1_LEFT;
+int m1Dir = +1;
+bool hitRightThisCycle = false;
+
+int m2 = M2_MIN;
+int m2Dir = +1;
+
+unsigned long lastMove = 0;
 const int stepDelayMs = 10;
 
-int motor2Angle = 90;
-const int motor2Step = 10;
-bool reachedRightThisCycle = false;
-
-bool paused = false;
-unsigned long pauseEndTime = 0;
-
-const int soundPin   = 2;
-const int redLed     = 8;
-const int triggerled = 7;
-
-int lastState = LOW;
-bool waitingSecond = false;
-unsigned long firstClapTime = 0;
-unsigned long lastEdgeTime = 0;
-
-const unsigned long debounceMs     = 40;
-const unsigned long doubleClapMs   = 300;
-const unsigned long pauseMs        = 3000;
-const unsigned long clapBlinkMs    = 50;
-
-const unsigned long espPulseMs     = 300;
-const unsigned long espGapMs       = 700;
-bool espPulseActive = false;
-bool espGapActive   = false;
-unsigned long espPulseEnd = 0;
-unsigned long espGapEnd   = 0;
-
-bool ledBlinkActive = false;
-unsigned long ledBlinkEnd = 0;
-
-unsigned long triggerLockoutEnd = 0;
-const unsigned long triggerLockoutMs = 1500;
-
-int shiftNumber = 0;
-unsigned long lastShiftTime = 0;
-const int shiftDelay = 90;
-
 void setup() {
-  pinMode(soundPin, INPUT);
+  Serial.begin(19200);
 
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(redLed, OUTPUT);
-  pinMode(triggerled, OUTPUT);
-
-  pinMode(stcp_Pin, OUTPUT);
-  pinMode(shcp_Pin, OUTPUT);
-  pinMode(ds_Pin, OUTPUT);
-
-  pinMode(espTriggerPin, OUTPUT);
-  digitalWrite(espTriggerPin, LOW);
-
-  motor1.attach(motorPin);
+  motor1.attach(motorPin1);
   motor2.attach(motorPin2);
 
-  motor1.write(currentAngle);
-  motor2.write(motor2Angle);
+  motor1.write(m1);
+  motor2.write(m2);
 
-  lastMoveTime = millis();
+  Serial.println("=== SERVO TEST (19200 baud) ===");
+  Serial.print("M1 range: "); Serial.print(M1_LEFT); Serial.print(".."); Serial.println(M1_RIGHT);
+  Serial.print("M2 range: "); Serial.print(M2_MIN);  Serial.print(".."); Serial.println(M2_MAX);
+  Serial.println("Motor2 steps after each full M1 cycle (left->right->left).");
 }
 
-void startClapBlink(unsigned long now) {
-  digitalWrite(LED_PIN, HIGH);
-  ledBlinkActive = true;
-  ledBlinkEnd = now + clapBlinkMs;
-}
+void stepMotor2() {
+  int old = m2;
 
-void updateClapBlink(unsigned long now) {
-  if (ledBlinkActive && now >= ledBlinkEnd) {
-    digitalWrite(LED_PIN, LOW);
-    ledBlinkActive = false;
-  }
-}
+  if (M2_MODE_BOUNCE) {
+    m2 += (m2Dir * M2_STEP);
 
-void startEspPulse(unsigned long now) {
-  digitalWrite(espTriggerPin, HIGH);
-  espPulseActive = true;
-  espGapActive = false;
-  espPulseEnd = now + espPulseMs;
-}
-
-void updateEspPulse(unsigned long now) {
-  if (espPulseActive && now >= espPulseEnd) {
-    digitalWrite(espTriggerPin, LOW);
-    espPulseActive = false;
-    espGapActive = true;
-    espGapEnd = now + espGapMs;
+    if (m2 >= M2_MAX) { m2 = M2_MAX; m2Dir = -1; }
+    if (m2 <= M2_MIN) { m2 = M2_MIN; m2Dir = +1; }
+  } else {
+    m2 += M2_STEP;
+    if (m2 > M2_MAX) m2 = M2_MAX;
+    if (m2 < M2_MIN) m2 = M2_MIN;
   }
 
-  if (espGapActive && now >= espGapEnd) {
-    espGapActive = false;
-  }
-}
+  motor2.write(m2);
 
-void handleServo(unsigned long now) {
-  if (paused) {
-    if (now >= pauseEndTime) {
-      paused = false;
-      digitalWrite(redLed, LOW);
-      digitalWrite(triggerled, LOW);
-    }
-    return;
-  }
-
-  if (now - lastMoveTime >= (unsigned long)stepDelayMs) {
-    currentAngle += direction;
-
-    if (currentAngle >= RIGHT) {
-      currentAngle = RIGHT;
-      direction = -1;
-      reachedRightThisCycle = true;
-    } else if (currentAngle <= LEFT) {
-      currentAngle = LEFT;
-      direction = 1;
-
-      if (reachedRightThisCycle) {
-        reachedRightThisCycle = false;
-        motor2Angle += motor2Step;
-        if (motor2Angle > RIGHT) motor2Angle = LEFT;
-      }
-    }
-
-    motor1.write(currentAngle);
-    motor2.write(motor2Angle);
-
-    lastMoveTime = now;
-  }
-}
-
-void handleClap(unsigned long now) {
-  int state = digitalRead(soundPin);
-
-  if (now - lastEdgeTime < debounceMs) {
-    lastState = state;
-    return;
-  }
-
-  if (state == HIGH && lastState == LOW) {
-    lastEdgeTime = now;
-    startClapBlink(now);
-
-    if (now < triggerLockoutEnd) {
-      lastState = state;
-      return;
-    }
-
-    if (waitingSecond && (now - firstClapTime <= doubleClapMs)) {
-      paused = true;
-      pauseEndTime = now + pauseMs;
-
-      digitalWrite(redLed, HIGH);
-      digitalWrite(triggerled, HIGH);
-
-      startEspPulse(now);
-      triggerLockoutEnd = now + triggerLockoutMs;
-      waitingSecond = false;
-    } else {
-      waitingSecond = true;
-      firstClapTime = now;
-    }
-  }
-
-  if (waitingSecond && (now - firstClapTime > doubleClapMs)) {
-    waitingSecond = false;
-  }
-
-  lastState = state;
-}
-
-void handleShiftRegister(unsigned long now) {
-  if (now - lastShiftTime >= (unsigned long)shiftDelay) {
-    digitalWrite(stcp_Pin, LOW);
-    shiftOut(ds_Pin, shcp_Pin, LSBFIRST, shiftNumber);
-    digitalWrite(stcp_Pin, HIGH);
-
-    shiftNumber++;
-    if (shiftNumber > 255) shiftNumber = 0;
-
-    lastShiftTime = now;
-  }
+  Serial.print("[CYCLE] M2: ");
+  Serial.print(old);
+  Serial.print(" -> ");
+  Serial.println(m2);
 }
 
 void loop() {
   unsigned long now = millis();
 
-  handleClap(now);
-  handleServo(now);
-  handleShiftRegister(now);
-  updateClapBlink(now);
-  updateEspPulse(now);
+  if (now - lastMove >= (unsigned long)stepDelayMs) {
+    m1 += m1Dir;
+
+    if (m1 >= M1_RIGHT) {
+      m1 = M1_RIGHT;
+      m1Dir = -1;
+      hitRightThisCycle = true;
+      Serial.println("[M1] Hit RIGHT, returning.");
+    }
+    else if (m1 <= M1_LEFT) {
+      m1 = M1_LEFT;
+      m1Dir = +1;
+
+      if (hitRightThisCycle) {
+        hitRightThisCycle = false;
+        stepMotor2();
+      }
+    }
+
+    motor1.write(m1);
+    lastMove = now;
+  }
 }
